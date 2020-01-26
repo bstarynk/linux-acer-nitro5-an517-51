a6a9c36819e08676885491e49427ba0aae5bb28d
Basile Starynkevitch <basile@starynkevitch.net>
2020-01-26 11:53:42 +0100

    adding sash

--- build.zsh
+++ build.zsh
@@ -10,7 +10,6 @@ LINUX_KERNEL_SRCDIR=/usr/src/Kernel/linux-5.4.14
 NBCORES=$(lscpu|awk '/^Core/{print $4}')
 
 function build_kernel() {
-    printf "you have %d cores\n" $NBCORES
     cp $LINUX_KERNEL_SRCDIR/.config my-$(basename $LINUX_KERNEL_SRCDIR)-config
     loadkeys -s -d > $LINUX_KERNEL_SRCDIR/drivers/tty/vt/defkeymap.c
     pushd $LINUX_KERNEL_SRCDIR
@@ -18,8 +17,17 @@ function build_kernel() {
     popd
 }
 
+function build_sash() {
+    pushd sash-3.8
+    make -j $[$NBCORES / 2] sash
+    popd
+}
 
 
+printf "%s: you have %d cores\n" $(realpath $0) $NBCORES
+
+build_sash
 build_kernel
+
 
 ## end of file build.zsh
---
+++ sash-3.8/cmd_basile.cpp
@@ -0,0 +1,57 @@
+// file cmd_basile.cpp is dual-licensed GPLv3+ or MIT
+
+/***
+// © Copyright 2020 by Basile Starynkevitch
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    This file "cmd_basile.cpp" is free software, you can use it,
+ *    modify it, and redistribute it either, and at your choice, under
+ *    GPLv3+ license above, or under MIT license below:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ***/
+
+#include <map>
+#include <vector>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+
+extern "C" {
+#include "sash.h"
+};
+
+int     do_curl(int argc, const char**argv) {
+}
