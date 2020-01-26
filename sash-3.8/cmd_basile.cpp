02e3a6b61f36bb67abce023c474e3538f6570b11
Basile Starynkevitch <basile@starynkevitch.net>
2020-01-26 13:39:03 +0100

    addinng basile_prompt

--- sash-3.8/cmd_basile.cpp
+++ sash-3.8/cmd_basile.cpp
@@ -43,15 +43,109 @@
 
 #include <map>
 #include <vector>
+#include <ofstream>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
+#include <curl.h>
 
 extern "C" {
 #include "sash.h"
 };
 
 int     do_curl(int argc, const char**argv) {
-}
+
+    std::clog << "unimplemented do_curl:";
+    for (int i=0; i<argc; i++)
+        std::clog << ' ' << argv[i];
+    std::clog << std::endl;
+    return 2;
+} // end do_curl
+
+
+char*
+basile_prompt(void)
+{
+    static std::string prstr;
+    std::string curprompt{prompt};
+    if (curprompt.empty()) {
+        curprompt = std::string((getuid()==0)?"sash#":"sash>");
+    }
+    else {
+        const char* pc = prompt;
+        const char* next = nullptr;
+        for (ix=0; ix>=0; ix++) {
+            pc = curprompt.c_str()+ix;
+            if (*pc == '\0')
+                break;
+            const char*percent = strchr(pc, '%');
+            if (percent) {
+                ix++;
+                if (percent[1] == '%')
+                {
+                    prstr += '%';
+                }
+                else if (percent[1] == '~')
+                {
+                    const char*cwd = get_current_dir_name();
+                    if (cwd) prstr += cwd;
+                    else prstr += '?';
+                    free (cwd);
+                }
+                else if (percent[1] == 'T')
+                {
+                    time_t nowt = time(nullptr);
+                    struct tm nowtm;
+                    char timbuf[80];
+                    memset(&tm, 0, sizeof(tm));
+                    memset(timbuf, 0, sizeof(timbuf));
+                    localtime_r(&nowt, &nowtm);
+                    strftime(timbuf, sizeof(timbuf), "%H:%M:%S %Z", nowtm);
+                    prstr += timbuf;
+                }
+                else if (percent[1] == 'D')
+                {
+                    time_t nowt = time(nullptr);
+                    struct tm nowtm;
+                    char timbuf[80];
+                    memset(&tm, 0, sizeof(tm));
+                    memset(timbuf, 0, sizeof(timbuf));
+                    localtime_r(&nowt, &nowtm);
+                    strftime(timbuf, sizeof(timbuf), "%Y/%b/%d", nowtm);
+                    prstr += timbuf;
+                }
+                else if (percent[1] == 't')
+                {
+                    char timbuf[32];
+                    memset (timbuf, 0, sizeof(timbuf));
+                    snprintf(timbuf, sizeof(timbuf), "%.2f", sash_time(CLOCK_PROCESS_CPUTIME_ID));
+                    prstr += timbuf;
+                }
+                else if (percent[1] == '$')
+                {
+                    char pidbuf[32];
+                    memset(pidbuf, 0, sizeof(pidbuf));
+                    snprintf(pidbuf, sizeof(pidbuf), "%ld", (long)getpid());
+                    prstr += pidbuf;
+                }
+                else if (percent[1] == '#') {
+                    if (getuid())
+                        prstr += "%";
+                    else
+                        prstr += "#";
+                }
+                else {
+                    prstr += percent[1];
+                }
+            }
+            else {// no percent
+                prstr += *pc;
+            }
+        }
+    }
+    if (!prstr.empty())
+        return prstr.c_str();
+    else return nullptr;
+} // end basile_prompt
